e0e5d79afe9b8fb38bda84e4ddcd9a45700975b8
Mine546272 <fondrycendranata@gmail.com>
2024-05-26 15:10:29 +0700

    updatedRecipePage

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,6 @@
 import React from "react";
 import MiniCard from "./MiniCard";
 
-
 const dietFoods = [
     { name: "Grilled Lemon Garlic Salmon" 
 
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,34 @@
+import MiniCard from "../components/MiniCard";
+import PageNav from "../components/PageNav";
+import MenuList from "../components/MenuList"
+import Addmenu from "../components/Addmenu";
 function Recipes() {
 
     return (
-        <h1>
-            Recipes
-        </h1>
+        <>
+        <nav>
+        <PageNav />
+        </nav>
+        <div className="centered-content">
+            
+            <body className="featured-recipes">
+            <header className="headerrecipe">
+                <h1 className="food-menu">Food Menu</h1>
+                <div className="addRecipee">
+                <Addmenu>Add Your Recipes</Addmenu>
+            </div>
+            </header>
+            
+            
+            
+
+            </body>
+        </div>
+        <div className="content-recipe">
+        <MenuList/>
+            </div>
+    
+        </>
     )
 }
 
